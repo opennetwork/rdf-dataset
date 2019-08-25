7e920942ca4cbef3a5d861f8310abf8068528774
Cloudswept <developer@cloudswept.com>
2019-08-25 17:19:28 +1200

    Utilise @opennetwork/dataset

--- src/iterator/iteratee.ts
+++
@@ -1,5 +0,0 @@
-import { Dataset, ResultType, ResultValue } from "../dataset";
-
-export interface IterateeFn<R extends ResultType, T, TLike, TFind, This, V = void, PV = Promise<V>> {
-  (this: This, data: T, dataset: Dataset<R, T, TLike, TFind>): ResultValue<R, V, PV>;
-}
