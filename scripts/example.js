7f3ea9fd140d7e5f0b89f60b346cae38a7696d4d
Fabian Cook <hello@fabiancook.dev>
2020-05-16 13:48:20 +1200

    Update example

--- scripts/example.js
+++ scripts/example.js
@@ -1,72 +1,42 @@
 import { Dataset } from "../esnext/index.js"
-import { addEventListener, getEnvironmentContext } from "@opennetwork/environment"
-import {
-  DefaultDataFactory
-} from "@opennetwork/rdf-data-model";
-import { asyncIterable } from "iterable"
+import { DefaultDataFactory } from "@opennetwork/rdf-data-model"
 
-addEventListener("configure", () => {
+const dataset = new Dataset()
 
-  const context = getEnvironmentContext()
+const aNameMatch = {
+  subject: DefaultDataFactory.blankNode("a"),
+  predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
+  graph: DefaultDataFactory.defaultGraph()
+}
 
-  context.store = new Dataset()
+const aMatcher = dataset.match(aNameMatch)
 
+dataset.add({
+  ...aNameMatch,
+  object: DefaultDataFactory.literal(`"A"@en`)
 })
 
-addEventListener("execute", async () => {
-
-  const context = getEnvironmentContext()
-
-  // console.log(await context.store.match({
-  //   subject: {
-  //     termType: "Variable",
-  //     value: "1"
-  //   }
-  // }).toSet())
-  // console.log(context.store.lazySet.has({}))
-
-  const artMatcher = context.store.match({
-    subject: DefaultDataFactory.blankNode("_:art"),
-    predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name")
-  })
-
-  context.store.add(DefaultDataFactory.fromQuad({
-    subject: DefaultDataFactory.blankNode("_:art"),
-    predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
-    object: DefaultDataFactory.literal(`"ArtA"@en`),
-    graph: DefaultDataFactory.defaultGraph()
-  }))
-
-  context.store.add(DefaultDataFactory.fromQuad({
-    subject: DefaultDataFactory.blankNode("_:smart"),
-    predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
-    object: DefaultDataFactory.literal(`"Smart"@en`),
-    graph: DefaultDataFactory.defaultGraph()
-  }))
-
-  console.log(artMatcher.size, context.store.size)
-
-  context.store.add(DefaultDataFactory.fromQuad({
-    subject: DefaultDataFactory.blankNode("_:art"),
-    predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
-    object: DefaultDataFactory.literal(`"ArtB"@en`),
-    graph: DefaultDataFactory.defaultGraph()
-  }))
-
-  console.log(artMatcher.size, context.store.size)
-
-  context.store.add(DefaultDataFactory.fromQuad({
-    subject: DefaultDataFactory.blankNode("_:art"),
-    predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
-    object: DefaultDataFactory.literal(`"ArtC"@en`),
-    graph: DefaultDataFactory.defaultGraph()
-  }))
-
-  console.log(artMatcher.size, context.store.size)
-
-  console.log(context.store.toArray())
-
+dataset.add({
+  subject: DefaultDataFactory.blankNode("s"),
+  predicate: DefaultDataFactory.namedNode("http://xmlns.com/foaf/0.1/name"),
+  object: DefaultDataFactory.literal(`"s"@en`),
+  graph: DefaultDataFactory.defaultGraph()
 })
 
-export default import("@opennetwork/environment/esnext/runtime/run.js")
-  .then(({ default: promise }) => promise)
+console.log({ a: aMatcher.size, total: dataset.size })
+
+dataset.add({
+  ...aNameMatch,
+  object: DefaultDataFactory.literal(`"B"@en`)
+})
+
+console.log({ a: aMatcher.size, total: dataset.size })
+
+dataset.add({
+  ...aNameMatch,
+  object: DefaultDataFactory.literal(`"C"@en`)
+})
+
+console.log({ a: aMatcher.size, total: dataset.size })
+console.log({ aObjects: aMatcher.toArray().map(({ object }) => object) })
+
--- scripts/example.node.js
+++ scripts/example.node.js
@@ -1,12 +1,1 @@
-import promise from "./example.js"
-
-promise
-  .then(result => {
-    console.log({ result })
-    console.log("complete. bye")
-    process.exit(0)
-  })
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+import "./example.js"
