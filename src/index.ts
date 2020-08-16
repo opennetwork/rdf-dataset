e397104b98b64fa68d874e6d05a6ac33b96fb112
Fabian Cook <hello@fabiancook.dev>
2020-08-16 21:45:49 +1200

    remove export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./match"
 export * from "./dataset"
-export * from "./immutable-dataset"
 export * from "./readonly-dataset"
 export * from "./set-like"
 export * from "./insert-like"
